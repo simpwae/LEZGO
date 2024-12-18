50bf4e62fce8260995e227a37e02beb87a736d91
simpwae <naqashamjad777@gmail.com>
2024-12-18 18:40:00 +0500

    secCommit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-res.send('Hello World!');
-})
-app.get('/mypage',(req,res)=>{
-    res.send('mypage');
-})
-app.listen(3000);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/mypage", (req, res) => {
+  res.send("mypage");
+});
+app.listen(process.env.PORT, () => {
+  console.log("server start");
+});
